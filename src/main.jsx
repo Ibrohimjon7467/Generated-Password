a0e6432922c3a999c2394381baf2f726c5234079
ibrohimjon7467 <Ibrohimjon7467@gmail.com>
2023-12-08 20:55:05 +0500

    none

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { toast, ToastContainer } from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
